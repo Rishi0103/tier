9294a5e61b4de52838f4f977035ff3c6b13e818a
root <root@ip-10-0-4-42.ec2.internal>
2025-05-09 06:01:13 +0000

    push

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-2.c7e6o0w04d6y.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Rishikesh',
+    DB_DATABASE : 'webappdb'
 });
